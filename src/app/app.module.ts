672f99d98b3c01b3aef91c7cc90f036addc6c74d
Giwoo Lee <gglee89@gmail.com>
2016-11-15 00:44:15 +0900

    Added checkout component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CheckoutComponent } from './checkout';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    CheckoutComponent
+],
   imports: [
     BrowserModule,
     FormsModule,
